acadea65ff0bbb6d8de6f02778434c75c7d39afa
carey-andrew <carey_andrew@hotmail.com>
2023-11-01 16:46:06 +0000

    server.js added PORT

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import { app } from "./app.js";
 
 const PORT = process.env.PORT;
 
+const HOST = process.env.HOST ?? '0.0.0.0';
 
-
-app.listen(PORT, function () {
+app.listen(PORT, HOST, function () {
     console.log("Port is running");
   });
